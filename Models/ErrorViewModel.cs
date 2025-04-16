93aef242c33f81e9b487acf83632b4fe6c189c18
Epihljak1 <epihljak1@etf.unsa.ba>
2025-04-16 15:40:31 +0200

    Univerzitet.MCV

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Univerzitet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
